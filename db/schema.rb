a88044820416d7b7e8dd0640ecf38825ebba4e03
Brage Gording <brage.gording@tngtech.com>
2026-03-16 17:30:55 +0000

    Revert citext column type for patient names
    - Revert collumn type change in commit 192fcc107
      - The citext collation does not work with the trigram (pg_tgrm) index
    - Instead of using ILIKE for case insensitive comparison use equality
      with lower()
      - This should hopefully keep the memory benefits of the citext type
        for this query

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_10_195113) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_16_165220) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "citext"
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pg_trgm"
 
@@ -742,9 +741,9 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_10_195113) do
     t.integer "ethnic_background"
     t.string "ethnic_background_other"
     t.integer "ethnic_group"
-    t.citext "family_name", null: false
+    t.string "family_name", null: false
     t.integer "gender_code", default: 0, null: false
-    t.citext "given_name", null: false
+    t.string "given_name", null: false
     t.bigint "gp_practice_id"
     t.boolean "home_educated"
     t.datetime "invalidated_at"
