4640ad51ea2e5c7cd0ca15025775a173c97f7b55
Jake Benilov <benilov@gmail.com>
2026-03-16 18:35:51 +0000

    Migrate vaccination_administered_flu email to local template
    Jira-Issue: MAV-4113

--- spec/features/flu_vaccination_administered_spec.rb
+++ spec/features/flu_vaccination_administered_spec.rb
@@ -331,9 +331,11 @@ describe "Flu vaccination" do
   end
 
   def then_an_email_is_sent_to_the_parent_confirming_the_vaccination
-    expect_email_to(
-      @patient.consents.last.parent.email,
-      :vaccination_administered_flu
+    expect(email_deliveries).to include(
+      matching_notify_email(
+        to: @patient.consents.last.parent.email,
+        template: :vaccination_administered_flu
+      ).with_content_including("Vaccination: flu", "Possible side effects")
     )
   end
 
