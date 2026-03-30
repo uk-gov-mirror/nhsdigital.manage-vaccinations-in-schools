pacharanero/manage-vaccinations-in-schools

Add `follow_up_outcome` and `follow_up_resolved_at` to consents
When a nurse follows up with a parent who requested a discussion before refusing, the outcome needs to be recorded on the existing consent. These columns track whether the follow-up resulted in the refusal being confirmed or withdrawn, and when that resolution took place - enabling the activity log to reflect the full follow-up history accurately.

db/migrate/20260317150000_add_follow_up_outcome_to_consents.rb
@@ -1,0 +1,10 @@
+# frozen_string_literal: true
+
+class AddFollowUpOutcomeToConsents < ActiveRecord::Migration[8.1]
+  def change
+    change_table :consents, bulk: true do |t|
+      t.string :follow_up_outcome
+      t.datetime :follow_up_resolved_at
+    end
+  end
+end

db/schema.rb
@@ -307,7 +307,9 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_18_112719) do
     t.bigint "consent_form_id"
     t.datetime "created_at", null: false
     t.enum "disease_types", null: false, array: true, enum_type: "disease_type"
+    t.string "follow_up_outcome"
     t.boolean "follow_up_requested"
+    t.datetime "follow_up_resolved_at"
     t.jsonb "health_answers", default: [], null: false
     t.datetime "invalidated_at"
     t.text "notes", default: "", null: false

uk-gov-mirror/nhsdigital.manage-vaccinations-in-schools

db/migrate/20260317150000_add_follow_up_outcome_to_consents.rb
@@ -1,0 +1,10 @@
+# frozen_string_literal: true
+
+class AddFollowUpOutcomeToConsents < ActiveRecord::Migration[8.1]
+  def change
+    change_table :consents, bulk: true do |t|
+      t.integer :follow_up_outcome
+      t.datetime :follow_up_resolved_at
+    end
+  end
+end

db/schema.rb
@@ -309,7 +309,9 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_30_075335) do
     t.bigint "consent_form_id"
     t.datetime "created_at", null: false
     t.enum "disease_types", null: false, array: true, enum_type: "disease_type"
+    t.integer "follow_up_outcome"
     t.boolean "follow_up_requested"
+    t.datetime "follow_up_resolved_at"
     t.jsonb "health_answers", default: [], null: false
     t.datetime "invalidated_at"
     t.text "notes", default: "", null: false