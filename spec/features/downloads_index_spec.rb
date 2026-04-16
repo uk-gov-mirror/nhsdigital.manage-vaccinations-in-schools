3e0dabeabd841fbf928032a9b88f927e5452bd26
Martin van IJcken <martin.vanijcken@tngtech.com>
2026-04-16 22:23:59 +0100

    Add location and offline session downloads index spec
    Jira-Issue: MAV-1521

--- spec/features/downloads_index_spec.rb
+++ spec/features/downloads_index_spec.rb
@@ -17,4 +17,85 @@ describe "Downloads index" do
       expect(page).not_to have_css(".nhsuk-card")
     end
   end
+
+  describe "location export card" do
+    context "when location is a clinic" do
+      before do
+        exportable = create(:location_export)
+        create(:export, exportable:, team:, user:)
+        visit downloads_path
+      end
+
+      it "does not show a school row" do
+        within(".nhsuk-card", text: "Community clinic offline session") do
+          expect(page).not_to have_css("dt", text: "School")
+        end
+      end
+    end
+
+    context "when location is a school" do
+      let(:school) { create(:gias_school, team:) }
+
+      before do
+        exportable = create(:location_export, location: school)
+        create(:export, exportable:, team:, user:)
+        visit downloads_path
+      end
+
+      it "shows the school name" do
+        within(".nhsuk-card", text: school.name) do
+          expect(page).to have_css("dt", text: "School")
+          expect(page).to have_content(school.name)
+        end
+      end
+    end
+  end
+
+  describe "offline session export card" do
+    let(:programme) { Programme.hpv }
+    let(:location) { create(:gias_school, team:, programmes: [programme]) }
+    let(:session) do
+      create(:session, team:, location:, programmes: [programme])
+    end
+
+    before do
+      exportable = create(:session_export, session:)
+      create(:export, exportable:, team:, user:)
+      visit downloads_path
+    end
+
+    it "shows the school name" do
+      within(".nhsuk-card", text: location.name) do
+        expect(page).to have_css("dt", text: "School")
+        expect(page).to have_content(location.name)
+      end
+    end
+  end
+
+  describe "type filter" do
+    let(:programme) { Programme.hpv }
+    let(:school) { create(:gias_school, team:, programmes: [programme]) }
+    let(:session) do
+      create(:session, team:, location: school, programmes: [programme])
+    end
+
+    before do
+      exportable = create(:location_export, location: school)
+      create(:export, exportable:, team:, user:)
+
+      exportable = create(:session_export, session:)
+      create(:export, exportable:, team:, user:)
+
+      visit downloads_path
+    end
+
+    it "shows only offline session exports when that filter is selected" do
+      choose "Offline session"
+      click_on "Update results"
+
+      within(".app-grid-column-results") do
+        expect(page).to have_content("#{school.name} offline session")
+      end
+    end
+  end
 end
