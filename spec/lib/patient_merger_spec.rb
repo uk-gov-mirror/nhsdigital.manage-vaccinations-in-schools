144f031f1e8680832566c51cdab6d0bfddd4dcc4
Lakshmi Murugappan <lakshmi.murugappan@tngtech.com>
2026-04-17 10:25:05 +0100

    Handle merging patients with change log entries
    This ensures that when merging two patients together, if the one to be
    destroyed has a change log entry, this is moved across to the other patient rather
    than preventing the merge from happening due to a foreign key
    constraint.

--- spec/lib/patient_merger_spec.rb
+++ spec/lib/patient_merger_spec.rb
@@ -73,6 +73,9 @@ describe PatientMerger do
     let(:patient_location) do
       create(:patient_location, session:, patient: patient_to_destroy)
     end
+    let(:patient_change_log_entry) do
+      create(:patient_change_log_entry, patient: patient_to_destroy)
+    end
     let(:patient_merge_log_entry) do
       create(:patient_merge_log_entry, patient: patient_to_destroy)
     end
@@ -194,6 +197,12 @@ describe PatientMerger do
 
     it "moves parent relationships" do
       expect { call }.to change { parent_relationship.reload.patient }.to(
+        patient_to_keep
+      )
+    end
+
+    it "moves patient change log entries" do
+      expect { call }.to change { patient_change_log_entry.reload.patient }.to(
         patient_to_keep
       )
     end
@@ -529,6 +538,7 @@ describe PatientMerger do
         important_notices
         notes
         parent_relationships
+        patient_change_log_entries
         patient_changesets
         patient_locations
         patient_specific_directions
