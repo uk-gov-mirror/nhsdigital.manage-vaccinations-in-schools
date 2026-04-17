70b93197fe310e1dba7744999b2a858074c629ce
Martin van IJcken <martin.vanijcken@tngtech.com>
2026-04-17 11:21:19 +0100

    Add school moves downloads index spec
    Jira-Issue: MAV-1521

--- spec/features/downloads_index_spec.rb
+++ spec/features/downloads_index_spec.rb
@@ -17,4 +17,53 @@ describe "Downloads index" do
       expect(page).not_to have_css(".nhsuk-card")
     end
   end
+
+  describe "school moves export card" do
+    before do
+      exportable = create(:school_moves_export)
+      create(:export, exportable:, team:, user:)
+      visit downloads_path
+    end
+
+    it "does not show a date range row when no dates set" do
+      expect(page).not_to have_content("Date range")
+    end
+
+    context "when date_from and date_to are set" do
+      before do
+        exportable =
+          create(
+            :school_moves_export,
+            date_from: Date.new(2024, 9, 1),
+            date_to: Date.new(2025, 7, 31)
+          )
+        create(:export, exportable:, team:, user:)
+        visit downloads_path
+      end
+
+      it "shows a date range row" do
+        expect(page).to have_content("Date range")
+        expect(page).to have_content("1 September 2024")
+        expect(page).to have_content("31 July 2025")
+      end
+    end
+  end
+
+  describe "type filter" do
+    before do
+      exportable = create(:school_moves_export)
+      create(:export, exportable:, team:, user:)
+
+      visit downloads_path
+    end
+
+    it "shows only school moves exports when that filter is selected" do
+      choose "School moves"
+      click_on "Update results"
+
+      within(".app-grid-column-results") do
+        expect(page).to have_content("School moves")
+      end
+    end
+  end
 end
