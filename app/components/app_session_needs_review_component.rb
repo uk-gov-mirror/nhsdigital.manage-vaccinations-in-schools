4a2c16766bb1b96e48e235c8e08a594d08d20133
Thomas Leese <thomas@leese.io>
2026-04-24 13:44:30 +0100

    Allow customising `AppSessionNeedsReviewComponent`
    This allows the users of this component to customise which items are
    shown. We need this as we want to customise which items are shown on
    each page.
    
    Jira-Issue: MAV-2570

--- app/components/app_session_needs_review_component.rb
+++ app/components/app_session_needs_review_component.rb
@@ -1,51 +1,71 @@
 # frozen_string_literal: true
 
 class AppSessionNeedsReviewComponent < ViewComponent::Base
-  def initialize(session)
+  def initialize(
+    session,
+    include_missing_nhs_numbers: true,
+    include_unmatched_responses: true
+  )
     @session = session
+    @include_missing_nhs_numbers = include_missing_nhs_numbers
+    @include_unmatched_responses = include_unmatched_responses
   end
 
   def call
     render AppWarningCalloutComponent.new(heading: "Needs review", level: 2) do
-      tag.ul do
-        safe_join(
-          items.map do |item|
-            tag.li { link_to(item[:text].call, item[:href].call) }
-          end
-        )
-      end
+      tag.ul { safe_join(list_items) }
     end
   end
 
-  def render? = items.present?
+  def render?
+    show_missing_nhs_numbers? || show_unmatched_responses?
+  end
 
   private
 
-  attr_reader :session
+  attr_reader :session,
+              :include_missing_nhs_numbers,
+              :include_unmatched_responses
 
   delegate :patients, to: :session
 
-  def items
-    @items ||= [children_without_nhs_number, unmatched_responses].compact
+  def list_items
+    [missing_nhs_numbers_item, unmatched_responses_item].compact.map do |item|
+      tag.li { link_to(item.fetch(:text), item.fetch(:href)) }
+    end
   end
 
-  def children_without_nhs_number
-    count = patients.without_nhs_number.count
-    return if count.zero?
+  def missing_nhs_numbers
+    @missing_nhs_numbers ||= patients.without_nhs_number.count
+  end
+
+  def show_missing_nhs_numbers?
+    include_missing_nhs_numbers && missing_nhs_numbers.positive?
+  end
+
+  def missing_nhs_numbers_item
+    return unless show_missing_nhs_numbers?
 
     {
-      text: -> { t("children_without_nhs_number", count:) },
-      href: -> { session_patients_path(session, missing_nhs_number: true) }
+      text: t("children_without_nhs_number", count: missing_nhs_numbers),
+      href: session_patients_path(session, missing_nhs_number: true)
     }
   end
 
   def unmatched_responses
-    count = ConsentForm.for_session(session).unmatched.count
-    return if count.zero?
+    @unmatched_responses ||= ConsentForm.for_session(session).unmatched.count
+  end
+
+  def show_unmatched_responses?
+    include_unmatched_responses && unmatched_responses.positive?
+  end
+
+  def unmatched_responses_item
+    return unless show_unmatched_responses?
 
     {
-      text: -> { t("unmatched_responses", count:) },
-      href: -> { consent_forms_path(session_slug: @session.slug) }
+      text: t("unmatched_responses", count: unmatched_responses),
+      href: consent_forms_path(session_slug: session.slug)
     }
   end
 end
--- spec/components/app_session_needs_review_component_spec.rb
+++ spec/components/app_session_needs_review_component_spec.rb
@@ -33,24 +33,57 @@ describe AppSessionNeedsReviewComponent do
 
       it { should have_text("1 child without an NHS number") }
       it { should have_link(href: /missing_nhs_number=true/) }
+
+      context "when not including this item" do
+        let(:component) do
+          described_class.new(session, include_missing_nhs_numbers: false)
+        end
+
+        it { should_not have_text("1 child without an NHS number") }
+        it { should_not have_link(href: /missing_nhs_number=true/) }
+      end
     end
 
     context "when session has multiple patients without an NHS number" do
       before { create_list(:patient, 3, nhs_number: nil, session:) }
 
       it { should have_text("3 children without an NHS number") }
+
+      context "when not including this item" do
+        let(:component) do
+          described_class.new(session, include_missing_nhs_numbers: false)
+        end
+
+        it { should_not have_text("3 children without an NHS number") }
+      end
     end
 
     context "when session has an unmatched consent response" do
       before { create(:consent_form, :recorded, session:) }
 
       it { should have_text("1 unmatched response") }
+
+      context "when not including this item" do
+        let(:component) do
+          described_class.new(session, include_unmatched_responses: false)
+        end
+
+        it { should_not have_text("1 unmatched response") }
+      end
     end
 
     context "when session has multiple unmatched consent responses" do
       before { create_list(:consent_form, 3, :recorded, session:) }
 
       it { should have_text("3 unmatched responses") }
+
+      context "when not including this item" do
+        let(:component) do
+          described_class.new(session, include_unmatched_responses: false)
+        end
+
+        it { should_not have_text("3 unmatched responses") }
+      end
     end
   end
 end
