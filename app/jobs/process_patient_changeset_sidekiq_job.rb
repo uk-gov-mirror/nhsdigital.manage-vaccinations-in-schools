931dee6a7a3e80d349ea2dd7b476a340aba5173f
Thomas Leese <thomas@leese.io>
2026-04-30 14:34:14 +0100

    Remove unused Sidekiq jobs
    This removes Sidekiq jobs which have been fully replaced and no longer
    exist on any queues.
    
    Jira-Issue: MAV-7288

--- app/jobs/process_patient_changeset_sidekiq_job.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-class ProcessPatientChangesetSidekiqJob < ProcessPatientChangesetJob
-end
--- app/jobs/send_school_consent_requests_sidekiq_job.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-class SendSchoolConsentRequestsSidekiqJob < SendSchoolConsentRequestsJob
-end
